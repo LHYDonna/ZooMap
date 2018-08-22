19aea10f430ab90f5d270e3f50db289b6f7ad743
林洪钰 <linhongyu@dyn-118-138-98-44.its.monash.edu.au>
2018-08-22 17:48:32 +1000

    Initial Commit

---
+++ Monash App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Monash App
+//
+//  Created by 林洪钰 on 22/8/18.
+//  Copyright © 2018 林洪钰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
